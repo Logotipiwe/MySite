df9a5023f70a939ae9aba22fc0284a851eda058b
Logotipiwe <41486972+Logotipiwe@users.noreply.github.com>
2018-08-13 18:31:54 +0300

    Released users registration

--- PHPposting.php
+++ PHPposting.php
@@ -3,7 +3,11 @@
 function printRes ($result){
             $cont = "";
                 while(($row = $result->fetch_assoc()) != false) {
-                    $cont = "<div class='post' id='Post".$row['id']."'><div class='postHead'><strong>".$row["name"]."</strong></div><div class='textCont'>".$row["content"]."</div><div class='postUnder'>Дата: ".$row["date"].". Автор:какойто чел.<div class = 'PostUnderDel' id = 'PostUnderDel".$row['id']."'>Удалить</div></div></div>".$cont;
+                    $del = "";
+                    if($row['Author'] == $_COOKIE["login"]){
+                        $del = "<div class = 'PostUnderDel' id = 'PostUnderDel".$row['id']."'>Удалить</div>";
+                    }
+                    $cont = "<div class='post' id='Post".$row['id']."'><div class='postHead'><strong>".$row["name"]."</strong></div><div class='textCont'>".$row["content"]."</div><div class='postUnder'>Дата: ".$row["date"].". Автор:".$row['Author'].".".$del."</div></div>".$cont;
                 } echo $cont;
             } 
 
@@ -12,14 +16,16 @@ if($_POST["action"] == 1){
         if($_POST["title"] != "" AND $_POST["content"] != ""){
             $sql = new mysqli("localhost","root", "", "logotipiwe");
             $sql->query ("SET NAMES 'UTF8'");
-            $sql->query ("INSERT INTO  `Switterposts` (
+            $sql->query ("INSERT INTO  `logotipiwe`.`switterposts` (
                 `id` ,
                 `name` ,
                 `content` ,
+                `Author` ,
                 `date`
-                )
+            )
                 VALUES (
-                NULL , '".$_POST["title"]."',  '".$_POST["content"]."', 
+                NULL , '".$_POST["title"]."',  '".$_POST["content"]."',
+                '".$_COOKIE['login']."',
                 CURRENT_TIMESTAMP
                 );");
             $sql->close();
@@ -34,7 +40,7 @@ if($_POST["action"] == 2){
             
             $sql = new mysqli("localhost","root", "", "logotipiwe");
             $sql->query ("SET NAMES 'UTF8'");
-            $result = $sql->query("SELECT * FROM Switterposts");
+            $result = $sql->query("SELECT * FROM Switterposts ORDER BY  `switterposts`.`id` ASC ");
             printRes($result);/*Вывод результата*/
             $sql->close();
 ?>
